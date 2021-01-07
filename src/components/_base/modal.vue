5e26df3af2adcf65431bee69681831ed5922131f
AgungWicakonoPutro <agungwicaksonoputro.dev@gmail.com>
2021-01-07 20:37:00 +0700

    firts commit

--- src/components/_base/modal.vue
+++ src/components/_base/modal.vue
@@ -8,7 +8,7 @@
             <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-md transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                 <button @click="$emit('event-close')" class="text-red-500 font-medium text-3xl hover:text-gray-800 focus:outline-none absolute top-2 right-3"><b-icon icon="x-circle"></b-icon></button>
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <form>
+                    <form class="mb-1">
                         <InputStarRatings v-model="data.star_rating"/>
                         <textarea v-model="data.comment" class=" bg-gray-300 mt-2 w-full py-3 px-5 resize-none placeholder-black focus:outline-none" name="comment" id="comment" cols="30" rows="4" placeholder="Tulis Review Terbaikmu"></textarea>
                         <div class="btn-group flex justify-between items-end">
@@ -17,6 +17,10 @@
                             <button @click="$emit('handle-update')" class="button w-36 h-10 bg-gray-300" id="button" type="button">Update</button>
                         </div>
                     </form>
+                    <div :class="[index %2 == 0 ? 'bg-red-300' : 'bg-red-400']" class="w-full flex p-1" v-for="(file, index) in data.images" :key="index">
+                        <div class="w-full">{{file.name}}</div>
+                        <div class="w-24"><button @click.prevent="data.images.splice(index, 1)">Delete</button></div>
+                    </div>
                     <div v-show="show" class="bg-yellow-200 text-red-400 my-3 p-2">{{message}}</div>
                 </div>
             </div>
